05af7869618a3ef9cebfda3eb3d73f6cb900ff26
armoin2018 <blaine@armoin.com>
2019-05-28 11:00:39 -0500

    Commit

--- setup_node.sh
+++ setup_node.sh
@@ -5,11 +5,11 @@ sudo apt-get install npm nodejs nodered --yes
 sudo npm cache clean -f
 sudo chown -R root:root /root/.npm /root/.node-gyp /root /usr/lib/node_modules
 sudo npm install npm -g  
-sudo npm install node-gyp --unsafe-perm -g
-sudo npm install usb  --unsafe-perm -g
-sudo npm install bluetooth-hci-socket --unsafe-perm -g
-sudo npm install bcrypt --unsafe-perm -g
-sudo npm install node-red-admin --unsafe-perm -g
+sudo npm install node-gyp --unsafe-perms -g
+sudo npm install usb  --unsafe-perms -g
+sudo npm install bluetooth-hci-socket --unsafe-perms -g
+sudo npm install bcrypt --unsafe-perms -g
+sudo npm install node-red-admin --unsafe-perms -g
 sudo npm install noble -g
 sudo npm install lodash -g
 sudo npm update -g
