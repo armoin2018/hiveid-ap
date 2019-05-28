8a9b2dd59443118fba109a4a33a09238027dff43
armoin2018 <blaine@armoin.com>
2019-05-28 15:13:39 -0500

    commit

--- setup_jmri.sh
+++ setup_jmri.sh
@@ -4,11 +4,15 @@ mkdir /home/pi/.config/autostart
 cp PanelPro.desktop /home/pi/.config/autostart/.
 #mkdir /home/pi/.config/lxsession /home/pi/.config/lxsession/LXDE-pi
 #cp autostart /home/pi/.config/lxsession/LXDE-pi/.
-sudo mkdir /opt/jmri
+sudo mkdir /opt/jmri /opt/jmri_backups 2>/dev/null
+cd /opt
+now=$(date +"%Y%m%d")
+sudo rm /opt/jmri_backups/jmri_$now.tar.gz
+sudo tar -zcvf /opt/jmri_backups/jmri_$now.tar.gz /opt/jmri
 cd /opt/jmri
+sudo rm -rdf *
 sudo wget https://github.com/JMRI/JMRI/releases/download/v4.14/JMRI.4.14+Rd060e0b.tgz
 sudo tar -xvf *.tgz
-sudo rm -rdf *
 sudo mv JMRI/* .
 sudo rm -rdf JMRI
 sudo chown -R pi:pi /opt/jmri /home/pi
