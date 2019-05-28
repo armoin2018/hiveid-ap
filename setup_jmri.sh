9f6e03f2c114eab3ad88955a7efee9b9f385d076
armoin2018 <blaine@armoin.com>
2019-05-28 15:18:54 -0500

    Commit

--- setup_jmri.sh
+++ setup_jmri.sh
@@ -7,12 +7,14 @@ cp PanelPro.desktop /home/pi/.config/autostart/.
 sudo mkdir /opt/jmri /opt/jmri_backups 2>/dev/null
 cd /opt
 now=$(date +"%Y%m%d")
-sudo rm /opt/jmri_backups/jmri_$now.tar.gz
-sudo tar -zcvf /opt/jmri_backups/jmri_$now.tar.gz /opt/jmri
+sudo rm /opt/jmri_backups/jmri_$now.tar.gz 2>/dev/null
+sudo tar -zcvf jmri_$now.tar.gz jmri
+mv jmri_$now.tar.gz /opt/jmri_backups/.
 cd /opt/jmri
 sudo rm -rdf *
 sudo wget https://github.com/JMRI/JMRI/releases/download/v4.14/JMRI.4.14+Rd060e0b.tgz
 sudo tar -xvf *.tgz
 sudo mv JMRI/* .
 sudo rm -rdf JMRI
+sudo rm /opt/jmri/*.tgz
 sudo chown -R pi:pi /opt/jmri /home/pi
