abf4ce9a67ef4cec3b04ef2cb5ae722451ef0091
armoin2018 <blaine@armoin.com>
2019-05-28 11:08:43 -0500

    Commit

--- setup_node.sh
+++ setup_node.sh
@@ -6,9 +6,11 @@ sudo npm cache clean -f
 sudo chown -R root:root /root/.npm /root/.node-gyp /root /usr/lib/node_modules
 sudo npm install npm -g  
 sudo npm install node-gyp --unsafe-perm -g
+sudo chown -R root:root /root/.npm /root/.node-gyp /root /usr/lib/node_modules
 sudo npm install usb  --unsafe-perm -g
 sudo npm install bluetooth-hci-socket --unsafe-perm -g
 sudo npm install bcrypt --unsafe-perm -g
+sudo chown -R root:root /root/.npm /root/.node-gyp /root /usr/lib/node_modules
 sudo npm install node-red-admin --unsafe-perm -g
 sudo npm install noble -g
 sudo npm install lodash -g
