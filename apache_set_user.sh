a856ca2d05656af1692a42933f50f74b800e878b
armoin2018 <blaine@armoin.com>
2019-05-27 21:50:15 -0500

    Commit

--- apache_set_user.sh
+++ apache_set_user.sh
@@ -2,6 +2,6 @@
 # Usage apache_set_user.sh USER
 USER=$1
 
-sed -i "s/^export APACHE_RUN_USER.*$/export APACHE_RUN_USER=$USER/g" /etc/apache2/envvars
-sed -i "s/^export APACHE_RUN_GROUP.*$/export APACHE_RUN_GROUP=$USER/g" /etc/apache2/envvars
-service apache2 restart
+sudo sed -i "s/^export APACHE_RUN_USER.*$/export APACHE_RUN_USER=$USER/g" /etc/apache2/envvars
+sudo sed -i "s/^export APACHE_RUN_GROUP.*$/export APACHE_RUN_GROUP=$USER/g" /etc/apache2/envvars
+sudo service apache2 restart
