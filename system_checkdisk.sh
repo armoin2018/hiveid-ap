913531c9315a4fa612a3ea844ab9b30640b99ae9
armoin2018 <blaine@armoin.com>
2019-06-20 15:13:24 -0500

    commit

---
+++ system_checkdisk.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+##########################################################
+#### Author: Blaine McDonnell (blaine@armoin.com)     ####
+#### Usage: ./system_checkdisk.sh [TRUE|FALSE]        ####
+#### Description: Runs checkdisk on the boot drive    ####
+#### Version: 0.20190620                              ####
+##########################################################
+IS_ENABLED=$1
+if [ "$IS_ENABLED" == "" ]; then
+    IS_ENABLED="TRUE"
+fi
+
+if [ "$IS_ENABLED" == "TURE" ]; then
+    sudo sed -i -e "s/$/ fsck.mode=force fsck.repair=yes/" /boot/cmdline.txt 
+else
+    sudo sed -i -e "s/\sfsck\.mode\=force\sfsck\.repair\=yes//" /boot/cmdline.txt 
+fi
