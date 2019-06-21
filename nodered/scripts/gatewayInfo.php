6518e55a0cf0b0bef32a95fd832d7ecca53f283f
armoin2018 <blaine@armoin.com>
2019-06-21 16:10:39 -0500

    commit

--- nodered/scripts/gatewayInfo.php
+++ nodered/scripts/gatewayInfo.php
@@ -36,17 +36,17 @@ if ($server == 'localhost') {
         foreach ($matches[1] as $items) {
             $temp = preg_split('/\=/',$items);
             if (!strcmp(trim($temp[0]),'network')) {
-                $ifaceID++;
-                preg_match('/\{[^\}]+\}/m',$items,$group);
-                $wpaNetLines = preg_split('/\n/',$group);
-                foreach ($wpaNetLines as $id=>$netLine) {
-                    $net_vals=  preg_split('/\=/',$netLine);
-                    $net_vals[1] = strtr($net_vals[1],"\"","");
-                    $myResults['wpa_net']['network'][$ifaceID][trim($net_vals[0])] = trim($net_vals[1]);
+                if (preg_match('/\{[^\}]+\}/m',$items,$group)) {
+                    $ifaceID++;
+                    $wpaNetLines = preg_split('/\n/',$group[1]);
+                    foreach ($wpaNetLines as $id=>$netLine) {
+                        $net_vals=  preg_split('/\=/',$netLine);
+                        $net_vals[1] = strtr($net_vals[1],"\"","");
+                        $myResults['wpa_net']['network'][$ifaceID][trim($net_vals[0])] = trim($net_vals[1]);
+                    }
                 }
-                
             } elseif (!empty($temp[1])) {
-                $myResults['wpa_net'][$temp[0]] = $temp[1];
+                $myResults['wpa_net'][trim($temp[0])] = trim($temp[1]);
             }
         }
     }
