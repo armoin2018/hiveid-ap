4dfa830891b75c1efd3208a62e7d5ed4f41564d4
armoin2018 <blaine@armoin.com>
2019-06-24 11:00:44 -0500

    commit

--- hiveid_set_binByUSB.sh
+++ hiveid_set_binByUSB.sh
@@ -3,14 +3,16 @@
 #### Author: Blaine McDonnell (blaine@armoin.com)                             ####
 #### Usage: ./hiveid_set_binByUSB.sh BinaryFile                               ####
 #### Description: Attempts to put the firmware on all attached USB devices    ####
-#### Version: 0.2                                                             ####
+#### Version: 0.20190624                                                      ####
+#### 	0.20190624	Moved esptool.py                                          ####
 ##################################################################################
 
 FILE=$1
 if [ -f $FILE ]; then
 	for PORT in $(ls /dev/ttyUSB*) 
-	do
-		python /usr/local/bin/esptool.py --port $PORT write_flash --flash_mode dio --flash_size detect 0x0 $FILE 2> /var/log/hiveid-ap/firmware.update.log
+	do	
+		echo "Trying to update $PORT with $FILE"
+		python /home/pi/.local/bin/esptool.py --port $PORT write_flash --flash_mode dio --flash_size detect 0x0 $FILE 2> /var/log/hiveid-ap/firmware.update.log
 	done
 else
 	echo "File $FILE does not exist."
