5769c13a858aa6a81dec53534f345280ed19fd2f
armoin2018 <blaine@armoin.com>
2019-07-09 10:09:38 -0500

    commit

--- nodered/scripts/gatewayInfo.php
+++ nodered/scripts/gatewayInfo.php
@@ -121,6 +121,7 @@ array_shift($routes);
 if (!empty($routes)) {
     $headerLine = array_shift($routes);
     $headers = preg_split('/\s/',$headerLine);
+    $myResults['routes'] = array();
     foreach ($routes as $line=>$routeLine) {
         if (!empty($routeLine)) {
             $myResults['routes'][] = array_combine($headers,preg_split('/\s/',$routeLine));
