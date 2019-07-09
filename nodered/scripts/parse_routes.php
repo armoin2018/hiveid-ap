cda6d0664a21f0cbac550a50fc0c8328036e6fe4
armoin2018 <blaine@armoin.com>
2019-07-09 10:20:08 -0500

    commit

--- nodered/scripts/parse_routes.php
+++ nodered/scripts/parse_routes.php
@@ -4,13 +4,20 @@ $myResults = array();
 $routes = preg_split('/\n/',trim(`route -v`));
 $myResults['routes_RAW'] = $routes;
 array_shift($routes);
+
 if (!empty($routes)) {
+    print_r($routes);
     $headerLine = array_shift($routes);
     $headers = preg_split('/[\s\t]/',$headerLine);
+    print_r($headers);
     $myResults['routes'] = array();
+    print_r($headers);
+    
     foreach ($routes as $line=>$routeLine) {
         if (!empty($routeLine)) {
-            $myResults['routes'][] = array_combine($headers,preg_split('/[\s\t]/',$routeLine));
+            $vals = preg_split('/[\s\t]/',$routeLine);
+            print_r($vals);
+            $myResults['routes'][] = array_combine($headers,$vals);
         }
     }
 }
