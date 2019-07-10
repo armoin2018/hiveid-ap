17c23061f81d39a182ed6e7827f7aff5d162d045
armoin2018 <blaine@armoin.com>
2019-07-10 12:46:30 -0500

    commit

--- hiveid_setup_client.sh
+++ hiveid_setup_client.sh
@@ -3,8 +3,9 @@
 #### Author: Blaine McDonnell (blaine@armoin.com)                                                   ####
 #### Usage: sudo ./hiveid_setup_client.sh                                                           ####
 #### Description: Sets up Raspberry Pi as a WiFi Client                                             ####
-#### Version: 0.20190708                                                                            ####
+#### Version: 0.20190710                                                                            ####
 ####          0.20190708    Initial Version                                                         ####
+####          0.20190710    Added Rule Removal                                                      ####
 ########################################################################################################
 
 # Stopping existing Services
@@ -19,3 +20,7 @@ sudo systemctl disable dnsmasq
 
 sudo rm /etc/iptables.ipv4.nat
 sudo rm /etc/network/if-post-up.d/zzz_hostapd
+
+# Remove Forwarding Rule and Firewall Rule loader
+sed -i -e "s/^net\.ipv4\.ip\_forward\=1$//g" /etc/sysctl.conf
+sed -i -e "s/iptables\-restore \< \/etc\/iptables\.ipv4\.nat//g" /etc/rc.local
