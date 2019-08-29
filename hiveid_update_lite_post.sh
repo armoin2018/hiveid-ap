7fa768e1f974093344d29f52ec8eff5d3f1546d2
armoin2018 <blaine@armoin.com>
2019-08-29 13:38:21 -0500

    commit

--- hiveid_update_lite_post.sh
+++ hiveid_update_lite_post.sh
@@ -3,9 +3,11 @@
 #### Author: Blaine McDonnell (blaine@armoin.com)     ####
 #### Usage: ./hiveid_update_lite_post.sh              ####
 #### Description: Updates HiveID's scripts w/o Node   ####
-#### Version:   20190829.001                          ####
+#### Version:   20190829.002                          ####
 ##########################################################
 
 sudo apt-get install -y qrencode 2>/dev/null
+mkdir /var/www/html/qr/ /usr/local/hiveid-ap/cache
+sudo chown -R pi:pi /var/www/html/qr /usr/local/hiveid-ap/cache
 /opt/hiveid-ap/setup_links.sh
 /opt/hiveid-ap/free_memory.sh
