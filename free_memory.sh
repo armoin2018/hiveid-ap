9c8e16d233c391dc97a6f36b49073c1ab583573c
armoin2018 <blaine@armoin.com>
2019-08-30 11:56:56 -0500

    commit

--- free_memory.sh
+++ free_memory.sh
@@ -6,5 +6,5 @@
 #### Version: 0.1                                     ####
 ##########################################################
 sudo sync 
-sudo echo 3 > /proc/sys/vm/drop_caches 2>/dev/null
+sudo sh -c "/usr/bin/echo 3 > /proc/sys/vm/drop_caches"
 sudo free -m
